9bb9f7936713b423b4dfb1d8cb2112ee9529f0fe
Rabyn-Collab <babynshrestha64@gmail.com>
2025-05-28 13:31:28 +0545

    chanages

--- lib/pages/meal_page.dart
+++ lib/pages/meal_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chaitra/pages/widgets/video_widget.dart';
 import 'package:flutter_chaitra/providers/meal_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,7 +17,21 @@ class MealPage extends ConsumerWidget {
       ),
       body: mealState.when(
           data: (data){
-            print(data);
+           return Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: ListView(
+               children: [
+
+                 PlayVideoFromYoutube(videoId: data['strYoutube'].split('=')[1] ?? ''),
+                 SizedBox(height: 10,),
+                Text(data['strMeal'] ?? ''),
+                 SizedBox(height: 10,),
+                 Image.network(data['strMealThumb'] ?? ''),
+                 SizedBox(height: 10,),
+                 Text(data['strInstructions'] ?? ''),
+               ],
+             ),
+           );
           },
           error: (err, st) => Text(err.toString()),
           loading: () => Center(child: CircularProgressIndicator(),)
